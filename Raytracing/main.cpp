4754f8a2a9d907ab586e461b50696af811e082c8
Ronan VALLÉE <rvallee@et.esiea.fr>
2021-01-28 16:13:22 +0100

    Preparation of main.c to display first a sphere

--- Raytracing/main.cpp
+++ Raytracing/main.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
 #include "FreeImage.h"
+#include "CScene.h"
 
 int main(int argc, char** argv)
 {
 	RGBQUAD color;
 	FIBITMAP* image;
 	image = FreeImage_Allocate(256, 256, 32);
+	CScene myScene;
+	//CSphere mySphere;
+
 
 	for (size_t i = 0; i < 640; i++)
 	{
 
 		for (size_t j = 0; j < 480; j++)
 		{
-			//if ()
-			color.rgbRed = 255;
-			color.rgbGreen = 255;
-			color.rgbBlue = 255;
+			if (true) // Quand la ray a une intersection avec notre objet (pour le moment sphere)
+			{
+				color.rgbRed = 255;
+				color.rgbGreen = 255;
+				color.rgbBlue = 255;
+			}
+			else
+			{
+				color.rgbRed = 0;
+				color.rgbGreen = 0;
+				color.rgbBlue = 0;
+			}
 
 			FreeImage_SetPixelColor(image, i, j, &color);
 		}
