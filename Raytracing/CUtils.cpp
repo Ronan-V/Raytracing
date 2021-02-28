4c4e38220708957fe0aae584514b82a2766d4970
brice <brice.laigo@gmail.com>
2021-02-28 21:53:58 +0100

    Add file parser

---
+++ Raytracing/CUtils.cpp
@@ -0,0 +1,145 @@
+﻿#include "CUtils.h"
+/* Constructors */
+
+/// <summary>
+/// Fetches the configuration from the input file, 
+/// </summary>
+/// <param name="p_input_file_path">The input file's path</param>
+CUtils::CUtils(std::string p_input_file_path) {
+	std::vector<std::string> configuration = get_words_from_file(p_input_file_path);
+	initialise_configuration(configuration);
+}
+
+/* Getters */
+
+/// <summary>
+/// Returns a list of CIntersectionObject pointers
+/// </summary>
+/// <returns>A list of CIntersectionObject pointers</returns>
+std::vector<CIntersectionObject*> CUtils::get_objects()
+{
+	return this->objects;
+}
+
+bool CUtils::get_isRelief()
+{
+	return this->isRelief;
+}
+
+/* IO */
+
+/// <summary>
+/// Reads a file and return a list of words contained in it
+/// </summary>
+/// <param name="p_file_path">The file's path to extract the words from</param>
+/// <returns>A list of strings representing the words contained in the file</returns>
+std::vector<std::string> CUtils::get_words_from_file(std::string p_file_path)
+{
+	std::vector<std::string> ret;
+	std::ifstream configFile(p_file_path);
+
+	std::string line;
+
+	if (configFile.is_open()) {
+		while (configFile >> line)
+		{
+			ret.push_back(line);
+		}
+
+		configFile.close();
+	}
+	else {
+		std::cerr << "Error while processing the input file '" << p_file_path << "'" << std::endl;
+	}
+
+	return ret;
+}
+
+/* Configuration */
+
+std::vector<std::string> CUtils::parse_line(std::string delimiter, std::string s) {
+	std::vector<std::string> ret = std::vector<std::string>();
+
+	size_t pos = 0;
+	std::string token;
+
+	for (size_t i = 0; i < 3; i++)
+	{
+		pos = s.find(delimiter);
+
+		token = s.substr(0, pos);
+		ret.push_back(token);
+		s.erase(0, pos + delimiter.length());
+	}
+
+	return ret;
+}
+
+Vector3D CUtils::parse_vector3D_from_line(std::string s) {
+	std::vector<std::string> coordinates = parse_line(",", s);
+
+	if (coordinates.size() != 3)
+	{
+		std::cerr << "Error, there are not 3 coordinates in the input'" << s << "'" << std::endl;
+		return NULL_VECTOR;
+	}
+
+	return NewVector(stof(coordinates[0]), stof(coordinates[1]), stof(coordinates[2]));
+}
+
+CIntersectionObject* CUtils::get_object_from_parsed_line(std::vector<std::string> parsed_line) {
+	CIntersectionObject* ret;
+
+	if (parsed_line[0] == "plan")
+	{
+		ret = new CPlan(parse_vector3D_from_line(parsed_line[1]), parse_vector3D_from_line(parsed_line[2]));
+	}
+	else if (parsed_line[0] == "sphere")
+	{
+		ret = new CSphere(parse_vector3D_from_line(parsed_line[1]), stof(parsed_line[2]));
+	}
+	else
+	{
+		std::cerr << "Error, object type not recognised '" << parsed_line[0] << "'" << std::endl;
+		return nullptr;
+	}
+
+	return ret;
+}
+
+/// <summary>
+/// Initialises the class members
+/// </summary>
+/// <param name="p_configuration"></param>
+void CUtils::initialise_configuration(std::vector<std::string> p_configuration) {
+	this->objects = std::vector<CIntersectionObject*>();
+	this->isRelief = false;
+
+	if (p_configuration[0] == "relief" && p_configuration[1] == "true")
+	{
+		this->isRelief = true;
+	}
+	int nbObjects = stoi(p_configuration[2]);
+
+
+	p_configuration.erase(p_configuration.begin(), p_configuration.begin() + 3);
+
+	for (int i = 0; i < nbObjects * 3; i += 3)
+	{
+		//std::vector<std::string> parsed_line = parse_line(" ", p_configuration[i]);
+		CIntersectionObject* ret;
+		if (p_configuration[i] == "plan")
+		{
+			ret = new CPlan(parse_vector3D_from_line(p_configuration[i + 1]), parse_vector3D_from_line(p_configuration[i + 2]));
+		}
+		else if (p_configuration[0] == "sphere")
+		{
+			ret = new CSphere(parse_vector3D_from_line(p_configuration[i + 1]), stof(p_configuration[i + 2]));
+		}
+		else
+		{
+			std::cerr << "Error, object type not recognised '" << p_configuration[i] << "'" << std::endl;
+		}
+		this->objects.push_back(ret);
+	}
+}
---
+++ Raytracing/CUtils.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "CIntersectionObject.h"
+#include "CPlan.h"
+#include "CSphere.h"
+#include "maths.h"
+
+class CUtils
+{
+private:
+	std::vector<CIntersectionObject*> objects;
+	bool isRelief;
+
+	void initialise_configuration(std::vector<std::string> p_configuration);
+public:
+	CUtils() = default;
+	~CUtils() = default;
+	CUtils(std::string p_input_file_path);
+	std::vector<CIntersectionObject*> get_objects();
+	bool get_isRelief();
+
+	std::vector<std::string> get_words_from_file(std::string p_file_path);
+	std::vector<std::string> parse_line(std::string delimiter, std::string s);
+	Vector3D parse_vector3D_from_line(std::string s);
+	CIntersectionObject* get_object_from_parsed_line(std::vector<std::string> parsed_line);
+};
